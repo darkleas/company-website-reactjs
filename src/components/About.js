a4d8d53a99147a91931ec2817cd63669644ab9c0
darkleas <58261666+darkleas@users.noreply.github.com>
2023-01-18 09:29:36 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <div className="container about">
         <div className="row">
           <div className="col-md-6 text-center">
-            <img alt="about" src="../img/img1.png" className="img-fluid" />
+            <img alt="about" src="./img/img1.png" className="img-fluid" />
           </div>
           <div className="col-md-6">
             <h2 className="main-title about-h2">ABOUT</h2>
